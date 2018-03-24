21e3a5ff68936b34a007dbe48addfbd907b116d1
Sanjay55 <sanjayvarma7506@gmail.com>
2018-03-25 00:05:07 +0530

    login controller

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['submit'])){
+	global $conn;
+	$username = $_POST['login'];
+	$password = $_POST['pwd'];
+	$username = mysqli_real_escape_string($conn,$username);
+	$password = mysqli_real_escape_string($conn,$password);
+	
+
+?>
