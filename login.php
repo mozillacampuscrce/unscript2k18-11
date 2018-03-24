a0c4219cd7e4a5b41b62a42abcf500c534559bb8
akky16596 <akky16596@gmail.com>
2018-03-25 03:09:51 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 if(isset($_POST['submit'])){
 	global $conn;
 	$username = $_POST['login'];
-	$password = $_POST['pwd'];
+	$password = $_POST['pwd']
 	$username = mysqli_real_escape_string($conn,$username);
 	$password = mysqli_real_escape_string($conn,$password);
 	
